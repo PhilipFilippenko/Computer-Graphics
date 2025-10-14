10c95cb7de33edd224528653d5433933ebb8bff4
Philip Filippenko <philip.filippenko@students.ittralee.ie>
2025-10-14 19:33:23 +0100

    Signed-off-by: Philip Filippenko <philip.filippenko@students.ittralee.ie>

--- Assets/GraphicsPipeline.cs
+++ Assets/GraphicsPipeline.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class GraphicsPipeline : MonoBehaviour
 {
@@ -16,24 +14,34 @@ public class GraphicsPipeline : MonoBehaviour
         myModel.CreateUnityGameObject();
         List<Vector3> verts3 = myModel.vertices;
 
-        List<Vector4> verts = convertToHOM(verts3);
-
-        writeVectorsToFile(verts, "Vertices of my letter", "-------------------");
+        List<Vector4> verts = ConvertToHOM(verts3);
+        WriteVectorsToFile(verts, "Vertices of my letter", "-------------------");
 
         Matrix4x4 rotationMatrix = Matrix4x4.TRS(Vector3.zero, Quaternion.AngleAxis(36, new Vector3(20, -5, -5).normalized), Vector3.one);
-        writeMatrixToFile(rotationMatrix, "Rotation Matrix", "------------------");
+        WriteMatrixToFile(rotationMatrix, "Rotation Matrix", "------------------");
 
-        List<Vector4> imageAfterRotation = applyTransformation(verts, rotationMatrix);
+        List<Vector4> imageAfterRotation = ApplyTransformation(verts, rotationMatrix);
+        WriteVectorsToFile(imageAfterRotation, "Image after Rotation", "------------------");
 
-        Matrix4x4 scaleMatrix =
-            Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(20, 5, 5));
+        Matrix4x4 scaleMatrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(20, 5, 5));
+        WriteMatrixToFile(scaleMatrix, "Scale Matrix", "------------------");
+
+        var imageAfterScale = ApplyTransformation(verts, scaleMatrix);
+        WriteVectorsToFile(imageAfterScale, "Image after Scale", "------------------");
+
+        Matrix4x4 translationMatrix = Matrix4x4.TRS(new Vector3(1f, 2f, -3f), Quaternion.identity, Vector3.one);
+        WriteMatrixToFile(translationMatrix, "Translation Matrix", "------------------");
+
+        var imageAfterTranslation = ApplyTransformation(verts, translationMatrix);
+        WriteVectorsToFile(imageAfterTranslation, "Image after Translation", "------------------");
 
         writer.Close();
-;    }
+    }
 
-    private List<Vector4> convertToHOM(List<Vector3> verts3)
+
+    private List<Vector4> ConvertToHOM(List<Vector3> verts3)
     {
-        List<Vector4> output = new List<Vector4>();
+        List<Vector4> output = new();
 
         foreach (Vector3 v in verts3)
         {
@@ -42,28 +50,27 @@ public class GraphicsPipeline : MonoBehaviour
         return output;
     }
 
-    private List<Vector4> applyTransformation(List<Vector4> verts, Matrix4x4 tranformMatrix)
+    private List<Vector4> ApplyTransformation(List<Vector4> verts, Matrix4x4 tranformMatrix)
     {
-        List<Vector4> output = new List<Vector4>();
+        List<Vector4> output = new();
         foreach (Vector4 v in verts)
         { output.Add(tranformMatrix * v); }
 
         return output;
     }
 
-    private void writeMatrixToFile(Matrix4x4 matrix, string before, string after)
+    private void WriteMatrixToFile(Matrix4x4 matrix, string before, string after)
     {
         writer.WriteLine(before);
-
         for (int i = 0; i < 4; i++)
         {
             Vector4 v = matrix.GetRow(i);
-            writer.WriteLine($"({v.x}, {v.y}, {v.z})");
+            writer.WriteLine($"({v.x}, {v.y}, {v.z}, {v.w})");
         }
         writer.WriteLine(after);
     }
 
-    private void writeVectorsToFile(List<Vector4> verts, string before, string after)
+    private void WriteVectorsToFile(List<Vector4> verts, string before, string after)
     {
         writer.WriteLine(before);
         foreach (Vector4 v in verts)
